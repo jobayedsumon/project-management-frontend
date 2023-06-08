7ff9baa4d9c482782960f7a71407d2a9926dace4
Jobayed Sumon <jobayed.sumon@thewickfirm.com>
2023-06-08 17:19:05 +0600

    redux toolkit added

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  user: null,
+  token: null,
+  isAuthenticated: false,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {},
+});
+
+export default authSlice.reducer;
